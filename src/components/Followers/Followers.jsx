070f246f2f943a95e5ccf3e85cb20463d5bd4850
fernanda rodriguez <fernandarpy@gmail.com>
2023-04-13 12:16:12 -0500

    implementacion componente redes sociales

--- src/components/Followers/Followers.jsx
+++ src/components/Followers/Followers.jsx
@@ -3,8 +3,19 @@ import "./followers.scss"
 
 const Followers = () => {
 return (
-    <div>
-        <h2>FOLLOWERS</h2>
+    <div className="main-followers">
+        <table>
+      <tr>
+        <td>Repos</td>
+        <td>Followers</td>
+        <td>Following</td>
+      </tr>
+      <tr>
+        <td>8</td>
+        <td>3938</td>
+        <td>9</td>
+      </tr>
+    </table>
     </div>
 )
 
--- src/components/SocialNetwork/SocialNetwork.jsx
+++ src/components/SocialNetwork/SocialNetwork.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import "./socialNetwork.scss"
+import "./socialNetwork.scss";
+import Location from "../../assets/location.svg";
+import Twiter from "../../assets/twiter.svg";
+import Link from "../../assets/link.svg";
+import Github from "../../assets/gitHub.svg";
 
-const socialNetwork = ()=>{
-return (
-    <div>
-        <h3>socialNetwork</h3>
+const socialNetwork = () => {
+  return (
+    <div className="main-socialNetwork">
+      <div>
+        <img src={Location} alt="" />
+        <p>san francisco</p>
+      </div>
+      <div>
+        <img src={Twiter} alt="" />
+        <p>Not Available</p>
+      </div>
+      <div>
+        <img src={Link} alt="" />
+        <p>https://github.blog</p>
+      </div>
+      <div>
+        <img src={Github} alt="" />
+        <p>@github</p>
+      </div>
     </div>
-    )
-
-}
+  );
+};
 
 export default socialNetwork;
